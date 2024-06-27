25609d292ac12bdf9b20689a8a592cb77eba54d9
Laquita Childress <LChildress@tql.com>
2024-06-27 11:13:22 -0400

    Created Item Class Created LC Branch Added Order Class

---
+++ LCandMikeProject/Model/Item.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LCandMikeProject.Model;
+
+public class Item {
+
+    public int Id { get; set; }
+    [StringLength(50)] 
+    public string Description { get; set; }
+    [Column(TypeName = "decimal(9,2)")]
+    public decimal Price { get; set; }
+
+
+}
