6d176eac1c07c8ff6f4f0de15979ce0ae4ee102b
Laquita Childress <LChildress@tql.com>
2024-06-27 11:13:22 -0400

    Added Order Class

---
+++ LCandMikeProject/Model/Order.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LCandMikeProject.Model;
+
+public class Order {
+
+    public int Id { get; set; }
+    public int CustomerId { get; set; }
+    [Column(TypeName = "datetime")]
+    public DateTime Date { get; set; } = DateTime.MinValue;
+    [StringLength(30)]
+    public string Description { get; set; } = string.Empty;
+    [StringLength(30)]
+    public string Status {  get; set; } = string.Empty;
+    [Column(TypeName = "decimal(9,2)")]
+    public decimal Total {  get; set; } 
+
+    public virtual Customer? Customer { get; set; }
+
+
+    
+}
